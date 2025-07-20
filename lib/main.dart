75c743850f1cef055bc09126a661363a7985c672
Asif Shaikh <sheikhaasif01@gmail.com>
2025-07-20 17:52:25 +0530

    fix: Add missing flutter/foundation import for kDebugMode
    - Add import 'package:flutter/foundation.dart' to main.dart
    - Fixes compilation error: Undefined name 'kDebugMode'
    - Required for Firebase App Check debug mode detection

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_app_check/firebase_app_check.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
